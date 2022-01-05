9f6415f0f66dc6d116465a0b879d74f29e8da54d
Reinaldo S.G <reinaldogunawan@yahoo.com>
2022-01-05 16:22:51 +0700

    add nodemon add broadcast emit

--- index.ts
+++ index.ts
@@ -2,23 +2,22 @@ let app  = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
+app.get('/', function(req:any, res:any){
     res.sendFile(__dirname + '/chat.html');
 });
 
-io.on('connection', function(socket){
+io.on('connection', function(socket:any){
     console.log('a user connected');
-    socket.on('joined', function(data) {
+    socket.on('joined', function(data:any) {
         console.log(data);
         socket.emit('acknowledge', 'Acknowledged');
     });
-    socket.on('chat message', function(msg){
-        console.log('message: ' + msg);
-        socket.emit('response message', msg + '  from server');
+    socket.on('chat message', function(msg:any){
+        io.emit('chat message', msg);
         //socket.broadcast.emit('response message', msg + '  from server');
     });
 });
 
-http.listen(8000, function(){
-    console.log('listening on *:8080');
+http.listen(3000, function(){
+    console.log('listening on *:3000');
 });
